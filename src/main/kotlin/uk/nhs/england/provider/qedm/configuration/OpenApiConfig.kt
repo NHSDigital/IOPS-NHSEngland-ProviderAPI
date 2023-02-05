65453a5154048a9ad7b484f14a8ffbafa52437e3
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2023-02-05 09:16:34 +0000

    Added test patient section

--- src/main/kotlin/uk/nhs/england/provider/qedm/configuration/OpenApiConfig.kt
+++ src/main/kotlin/uk/nhs/england/provider/qedm/configuration/OpenApiConfig.kt
@@ -25,8 +25,8 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 open class OpenApiConfig(@Qualifier("R4") val ctx : FhirContext) {
 
-    var HEALTH_ADMIN = "Health Admin"
-    var DOCUMENTS = "Documents"
+    var HEALTH_ADMIN = "Health Administration"
+    var DOCUMENTS = "Clinical Documents"
 
     @Bean
     open fun customOpenAPI(
@@ -40,15 +40,51 @@ open class OpenApiConfig(@Qualifier("R4") val ctx : FhirContext) {
                     .title(fhirServerProperties.server.name)
                     .version(fhirServerProperties.server.version)
                     .description(
-                                "## FHIR Implementation Guides"
-                                + "\n\n [UK Core Implementation Guide (0.5.1)](https://simplifier.net/guide/ukcoreimplementationguide0.5.0-stu1/home?version=current)"
-                                + "\n\n [NHS England Implementation Guide (2.6.0)](https://simplifier.net/guide/nhsdigital?version=2.6.0)"
+                        "All implementations and NHS England projects **MUST** adhere to NHS England profile and test this using [FHIR Validation](http://hl7.org/fhir/R4/validation.html). These profiles are all derived fro UKCore and so it is not necessary to test against UKCore."
+                                + "\n\n The scope of this profile is **England National** and does not the English NHS at local or regional level."
+                                + "\n" +
+                                "\n | HL7 FHIR | UK Core | NHS England |"
+                                + "\n |-----|------|-----|"
+                                + "\n | [Appointment](http://hl7.org/fhir/R4/appointment.html)| | [NHSDigital-Appointment](https://simplifier.net/guide/NHSDigital/Home/FHIRAssets/AllAssets/Profiles/NHSDigital-Appointment.guide.md?version=current) |"
+                                + "\n | [ServiceRequest](http://hl7.org/fhir/R4/servicerequest.html)| | [NHSDigital-ServiceRequest](https://simplifier.net/guide/NHSDigital/Home/FHIRAssets/AllAssets/Profiles/NHSDigital-ServiceRequest.guide.md?version=current) |"
+                                + "\n | [Task](http://hl7.org/fhir/R4/task.html)| | [NHSDigital-Task](https://simplifier.net/guide/NHSDigital/Home/FHIRAssets/AllAssets/Profiles/NHSDigital-Task.guide.md?version=current) |" +
+
+                        "\n\n ## FHIR Implementation Guides"
+                                + "\n\n All implementations and NHS England projects **SHOULD** aim to adhere to the **current** version of the following implementation guides."
+                                + "\n\n [UK Core Implementation Guide](https://simplifier.net/HL7FHIRUKCoreR4)"
+                                + "\n\n [NHS England Implementation Guide](https://simplifier.net/guide/nhsdigital)"
                                         + "\n\n ### Test Patients "
+                                        + "\n\n This server is preloaded with data from several NHS England projects. The following NHS Numbers contain examples: "
                                         + "\n\n | NHS Number | Patient Name | Data Source | "
                                         + "\n |---|---|---|"
                                         + "\n |9000000009|Julie Jones|eRS|"
                                         + "\n |9432003812|John Reardon|Pathology/Genomics|"
-                                        + "\n |9876543210|Rachel Kanfeld|EPS|"
+                                        + "\n |9876543210|Rachel Kanfeld|EPS|" +
+                        "\n\n ## NHS England API Documentation"
+                                + "\n\n | API | NHS England API | Provider API | Notes | "
+                                + "\n |---|---|---|---|"
+                                + "\n | [Booking and Referral - FHIR API](https://digital.nhs.uk/developer/api-catalogue/booking-and-referral-fhir) | X | X | See `Get referrals for a patient` and `Get bookings for a patient` |"
+                                + "\n | [Electronic Prescription Service - FHIR API](https://digital.nhs.uk/developer/api-catalogue/electronic-prescription-service-fhir) | X | - | See `Search for summary details about a prescription` | "
+                                + "\n | [e-Referral Service Patient Care – FHIR API](https://digital.nhs.uk/developer/api-catalogue/e-referral-service-patient-care-fhir) | X | - | See `Retrieve referral requests for a patient`. This API is a provider API for Wayfinder | "
+                                + "\n | [Patient Care Aggregator - FHIR API](https://digital.nhs.uk/developer/api-catalogue/patient-care-aggregator-fhir) | - | X | AKA Wayfinder. The provider API is not currently documented | "
+                                + "\n | GP Connect Prescription Management | X | - | Check status of this API | "
+                                + "\n | [National Record Locator - FHIR API]() | X | X | FHIR R4 version is in progress. This documentation only covers retrieval of documents, clinical data is covered elsewhere. | "
+                        + "\n\n ## API Cross Reference"
+                                + "\n\n ### Provider API"
+                                + "\n\n | API | Appointment | Binary | DocumentReference | ServiceRequest | Task | "
+                                + "\n |---|---|---|---|----|-----|"
+                                + "\n | Booking and Referral - FHIR API | X |  |  |  |  |"
+                                + "\n | Patient Care Aggregator - FHIR API | X | in-progress | in-progress  | X | in-progress | "
+                                + "\n | National Record Locator - FHIR API |   | X |    |   |  | "
+                                + "\n\n ### NHS England API"
+                                + "\n\n | API | Appointment | Binary | DocumentReference | ServiceRequest | Task | "
+                                + "\n |---|---|---|---|----|-----|"
+                                + "\n | Booking and Referral - FHIR API | X? |  |  | X |  |"
+                                + "\n | Electronic Prescription Service - FHIR API |  |  |  |  | X | "
+                                + "\n | e-Referral Service Patient Care – FHIR API | future | future | future  | X | future | "
+                                + "\n | GP Connect Prescription Management |  |  |  |  | X | "
+                                + "\n | National Record Locator - FHIR API |   |  | X   |   |  | "
+                                + "\n\n It is believed the scope of these API's could be extended to support regional interop. This would involve adding API's covering `Encounter`, `EpisodeOfCare`, `Patient`, `Schedule` and `Slot`. A high level view of this is contained in the [HL7 FHIR Administration Module](https://www.hl7.org/fhir/r4/administration-module.html)."
                     )
                     .termsOfService("http://swagger.io/terms/")
                     .license(License().name("Apache 2.0").url("http://springdoc.org"))
@@ -173,7 +209,7 @@ open class OpenApiConfig(@Qualifier("R4") val ctx : FhirContext) {
                 Operation()
                     .addTagsItem(HEALTH_ADMIN)
                     .summary("Search ServiceRequest (Local/Regional: BARS?, National: eRS(for Wayfinder))")
-                    .description("BARS eRS(Wayfinder)")
+                    .description("")
                     .responses(getApiResponses())
                     .addParametersItem(Parameter()
                         .name("patient:identifier")
