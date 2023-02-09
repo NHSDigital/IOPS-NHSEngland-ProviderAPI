b4b1681a0df75973b262153f8c3dea3db70b4d18
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2023-02-09 17:08:28 +0000

    Binary url fix

--- src/main/kotlin/uk/nhs/england/provider/qedm/provider/DocumentReferenceProvider.kt
+++ src/main/kotlin/uk/nhs/england/provider/qedm/provider/DocumentReferenceProvider.kt
@@ -7,12 +7,16 @@ import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException
 import org.hl7.fhir.r4.model.*
 import org.springframework.stereotype.Component
 import uk.nhs.england.provider.qedm.awsProvider.AWSPatient
+import uk.nhs.england.provider.qedm.configuration.FHIRServerProperties
 import uk.nhs.england.provider.qedm.interceptor.CognitoAuthInterceptor
+import uk.nhs.england.provider.qedm.util.FhirSystems
 import javax.servlet.http.HttpServletRequest
 
 @Component
 class DocumentReferenceProvider(var cognitoAuthInterceptor: CognitoAuthInterceptor,
-val awsPatient: AWSPatient) : IResourceProvider {
+val awsPatient: AWSPatient,
+                              val  fhirServerProperties: FHIRServerProperties
+) : IResourceProvider {
     override fun getResourceType(): Class<DocumentReference> {
         return DocumentReference::class.java
     }
@@ -20,7 +24,21 @@ val awsPatient: AWSPatient) : IResourceProvider {
     @Read
     fun read(httpRequest : HttpServletRequest, @IdParam internalId: IdType): DocumentReference? {
         val resource: Resource? = cognitoAuthInterceptor.readFromUrl(httpRequest.pathInfo, null)
-        return if (resource is DocumentReference) resource else null
+        return if (resource is DocumentReference) fixUrl(resource as DocumentReference) else null
+    }
+
+    fun fixUrl(documentReference: DocumentReference) : DocumentReference {
+        if (documentReference.hasContent() ) {
+            for (content in documentReference.content) {
+                if (content.hasAttachment() && content.attachment.hasUrl()) {
+                    if (content.attachment.url.startsWith("http://localhost:")) {
+                        var urls = content.attachment.url.split("Binary")
+                        if (urls.size>1) content.attachment.url = fhirServerProperties.server.baseUrl + "/FHIR/R4"+ urls[1]
+                    }
+                }
+            }
+        }
+        return documentReference
     }
    
     @Search
@@ -36,7 +54,11 @@ val awsPatient: AWSPatient) : IResourceProvider {
             val resource: Resource? = cognitoAuthInterceptor.readFromUrl(httpRequest.pathInfo, "patient="+patient.idElement.idPart)
             if (resource != null && resource is Bundle) {
                 for (entry in resource.entry) {
-                    if (entry.hasResource() && entry.resource is DocumentReference) documents.add(entry.resource as DocumentReference)
+                    if (entry.hasResource() && entry.resource is DocumentReference) {
+                        val documentReference = fixUrl(entry.resource as DocumentReference)
+
+                        documents.add(documentReference)
+                    }
                 }
             }
         }
